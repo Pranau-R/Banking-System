38a9da3af1a08d3f19fcc3f5b325e0054bc568bd
PranauR <ajaypranauer10@gmail.com>
2022-04-18 18:43:11 +0530

    #1:completed Source

--- src/banking_system.c
+++ src/banking_system.c
@@ -17,6 +17,7 @@ Author:
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<math.h>
 
 struct user
     {
@@ -51,16 +52,7 @@ Returns:
 
 void main()
     {
-    //struct user user;
-    //char num[50];
-    //char cash[50];
     int ch;
-    //printf("\n---You selected Account Details---\n\n");
-
-    /*printf("\nEnter your Registered Mobile Number:\n");
-    scanf("%s", num);
-    printf("the num is:\n", num);*/
-    //display(num);
 
     printf("\n");
     printf("--------------------------------------------------------------------------------\n");
@@ -81,7 +73,7 @@ void main()
         printf("5. Close Account: \n");
         printf("6. Exit: \n");
         printf("\n");
-    
+
         printf("\nEnter the Choice: \n");
         scanf("%d", &ch);
 
@@ -146,9 +138,7 @@ void open ()
     FILE *fp;
     struct user user;
     char filename[12];
-    user.balance = 0;
-
-    //printf("Balance is: %lf\n", user.balance);    
+    user.balance = 0;    
 
     system("cls");
 
@@ -156,11 +146,9 @@ void open ()
 
     printf("\n\nEnter Name:\t");
     scanf("%s", user.name);
-    //printf("\n\nName is: %s\n", user.name);
 
     printf("\n\nEnter Mobile Number:\t");
     scanf("%s", user.mob);
-    //printf("\n\nNumber is: %s\n", user.mob);
 
     printf("\n\nEnter Account Type:\t");
     scanf("%s", user.type);
@@ -170,13 +158,11 @@ void open ()
 
     strcpy(filename, user.mob);
 
-    fp = fopen(strcat(filename, ".txt"), "w");
-
-    //fprintf(fp,"\n Username : %s \n Mobile Number : %s \n Account Type : %s \n Password : %s \n Balance : %lf", user.name, user.mob, user.type, user.pass, user.balance);
+    fp = fopen(strcat(filename, ".txt"), "wb");
 
     fwrite(&user, sizeof(user), 1, fp);
 
-    if(fprintf > 0)
+    if(fwrite != 0)
         {
         printf("\nAccount Succesfully Created!\n\n");
         }
@@ -211,13 +197,14 @@ void display()
     char passwd[15];
 
     printf("\n---You selected Account Details---\n\n");
+
     printf("\nEnter your Registered Mobile Number:\n");
     scanf("%s", num);
 
     printf("\nEnter your Password:\n");
     scanf("%s", passwd);
 
-    fp = fopen(strcat(num, ".txt"), "r");
+    fp = fopen(strcat(num, ".txt"), "rb");
 
     if (fp == NULL)
         {
@@ -226,6 +213,7 @@ void display()
     else
         {
         fread(&user, sizeof(user), 1, fp);
+
         if (strcmp (passwd, user.pass) == 0)
             {
             system("cls");
@@ -242,19 +230,16 @@ void display()
             printf("\nAccount Type: %s", user.type);
 
             printf("\nBalance: %.2lfRs\n\n", user.balance);
-
-            printf("Press any key to Display Main Menu...!\n\n");
-            getch();
             }
         else
             {
             printf("\nIncorect Password!!.\n");
-
-            printf("Press any key to Display Main Menu...!\n\n");
-            getch();
             }
         }
     fclose(fp);
+
+    printf("Press any key to Display Main Menu...!\n\n");
+    getch();
     }
 
 /****************************************************************************\
@@ -277,18 +262,19 @@ void deposit()
     {
     FILE *fp;
     struct user user;
-    int amount;
+    double amount;
     char num[12];
     char passwd[15];
 
     printf("\n---You Selected Cash Deposit---\n\n");
+
     printf("\nEnter your Registered Mobile Number:\n");
     scanf("%s", num);
 
     printf("\nEnter your Password:\n");
     scanf("%s", passwd);
 
-    fp = fopen(strcat(num, ".txt"), "r+");
+    fp = fopen(strcat(num, ".txt"), "rb+");
 
     if (fp == NULL)
         {
@@ -297,6 +283,7 @@ void deposit()
     else
         {
         fread(&user, sizeof(user), 1, fp);
+
         if (strcmp(passwd, user.pass) == 0)
             {
             deposit:
@@ -306,11 +293,11 @@ void deposit()
             printf("Hello! %s \n", user.name);
 
             printf("Enter the amount to be deposited:\n");
-            scanf("%d", &amount);
+            scanf("%lf", &amount);
 
-            if (amount % 500 != 0)
+            if (fmod(amount, 500) != 0)
                 {
-                printf("\nSorry... The amount should be multiple of 500");
+                printf("\nSorry... The amount should be a multiple of 500");
                 printf("\nPress any key to redirect to CASH DEPOSIT!...");
 
                 getch();
@@ -320,11 +307,12 @@ void deposit()
                 {
                 user.balance += amount;
 
-                fwrite(&user,sizeof(struct user),1,fp);
+                fseek(fp, 0, SEEK_SET);
+                fwrite(&user, sizeof(struct user), 1, fp);
 
                 if(fwrite != 0)
                     {
-                    printf("\n\nYou have successfully depostied Rs.%d",amount);
+                    printf("\n\nYou have successfully depostied Rs.%.2lf",amount);
                     printf("\nAvailable Balance: %.2lfRs\n", user.balance);
                     }
                 }
@@ -360,7 +348,7 @@ void withdraw ()
     {
     FILE *fp;
     struct user user;
-    int amount;
+    double amount;
     char num[12];
     char passwd[15];
 
@@ -372,7 +360,7 @@ void withdraw ()
     printf("\nEnter your Password:\n");
     scanf("%s", passwd);
 
-    fp = fopen(strcat(num, ".txt"), "r+");
+    fp = fopen(strcat(num, ".txt"), "rb+");
 
     if (fp == NULL)
         {
@@ -381,6 +369,7 @@ void withdraw ()
     else
         {
         fread(&user, sizeof(user), 1, fp);
+
         if (strcmp(passwd, user.pass) == 0)
             {
             withdraw:
@@ -391,20 +380,26 @@ void withdraw ()
 
             printf("Available Balance: %.2lfRs \n", user.balance);
             printf("Enter the amount to be withdrawn:\n");
-            scanf("%d", &amount);
+            scanf("%lf", &amount);
 
-            if (amount % 500 != 0)
+            if (user.balance < 500)
+                {
+                printf("\nSorry, Your balance is insufficient for WITHDRAWL!");
+                }
+            else if (fmod(amount, 500) != 0)
                 {
                 printf("\nSorry... The amount should be multiple of 500");
-                printf("\nPress any key to redirect to CASH WITHDRAWL!...");
+                printf("\n\nPress any key to redirect to CASH WITHDRAWL!...");
 
                 getch();
                 goto withdraw;
                 }
             else if (amount > user.balance)
                 {
-                printf("\nSorry insufficient balance");
-                printf("\nPress any key to redirect to CASH WITHDRAWL!...");
+                printf("\nSorry insufficient balance!");
+                printf("\nWithdraw an amount lesser than available balance...");
+                printf("\n\nPress any key to redirect to CASH WITHDRAWL!...");
+
                 getch();
                 goto withdraw;
                 }
@@ -412,11 +407,12 @@ void withdraw ()
                 {
                 user.balance -= amount;
 
+                fseek(fp, 0, SEEK_SET);
                 fwrite(&user, sizeof(struct user), 1, fp);
 
                 if(fwrite !=0)
                     {
-                    printf("\n\nYou have sucessfully withdrawn Rs.%d",amount);
+                    printf("\n\nYou have sucessfully withdrawn Rs.%.2lf",amount);
                     printf("\nAvailable Balance: %.2lfRs \n", user.balance);
                     }
                 }
@@ -451,6 +447,7 @@ void close()
     int choice; 
 
     printf("\n---You Selected Account Deletion---\n");
+
     printf("\nEnter your Registered Mobile Number:\n");
     scanf("%s", num);
 
@@ -466,6 +463,7 @@ void close()
     else
         {
         fread(&user, sizeof(user), 1, old);
+
         if (strcmp(passwd, user.pass) == 0)
             {
             system("cls");
@@ -485,28 +483,28 @@ void close()
                 case 1:
                     {
                     remove (num);
-                    printf("\n\n Your Account DELETED successfully!\n\n");
+                    printf("\n\n Your Account DELETED successfully!\n");
                     break;
                     }
                 case 2:
                     {
-                    printf("\nAccount deletion cancelled!!\n\n");
+                    printf("\nAccount DELETION cancelled!!\n");
                     break;
                     }
                 default:
                     {
                     printf("\nInvalid Entry!!\n\n");
-                    printf("\nAccount deletion request is cancelled\n\n");
+                    printf("\nAccount DELETION request is cancelled!\n");
                     break;
                     }
                 }
             }
         else
             {
-            printf("\nIncorect Password!!.\n");
+            printf("\nIncorect Password!!\n");
             }
         }
 
-    printf("\nPress any key to Display Main Menu...!\n\n");
+    printf("\nPress any key to Display Main Menu...!\n");
     getch();
     }
